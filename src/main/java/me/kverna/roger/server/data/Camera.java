fc917aa9b46b1c8878c047c0732e110e29325084
Per Christian Kvalvik <pc@kvalvik.no>
2019-10-15 15:36:02 +0200

    Add Camera data entity and repository.

---
+++ src/main/java/me/kverna/roger/server/data/Camera.java
@@ -0,0 +1,28 @@
+package me.kverna.roger.server.data;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Camera {
+
+    private @Id
+    @GeneratedValue
+    Integer id;
+    private String name;
+    private String description;
+    private String host;
+}
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/me/kverna/roger/server/data/CameraRepository.java
@@ -0,0 +1,7 @@
+package me.kverna.roger.server.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CameraRepository extends JpaRepository<Camera, Integer> {
+
+}
