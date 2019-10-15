6d5570be72693b23526fa4c54c2c4526df48a09f
Per Christian Kvalvik <pc@kvalvik.no>
2019-10-15 15:49:38 +0200

    Restructure code.

--- src/main/java/me/kverna/roger/server/data/Camera.java
+++ src/main/java/me/kverna/roger/server/data/Camera.java
@@ -10,9 +10,9 @@ import javax.persistence.Id;
 @Entity
 public class Camera {
 
-    private @Id
+    @Id
     @GeneratedValue
-    Integer id;
+    private Integer id;
     private String name;
     private String description;
     private String host;
