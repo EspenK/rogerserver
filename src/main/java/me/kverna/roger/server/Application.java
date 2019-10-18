068c04c9320342c71a367aacc7b57d8fa1deeace
Per Christian Kvalvik <pc@kvalvik.no>
2019-10-18 12:24:56 +0200

    Setup run method for running the service in the future

--- src/main/java/me/kverna/roger/server/Application.java
+++ src/main/java/me/kverna/roger/server/Application.java
@@ -1,12 +1,21 @@
 package me.kverna.roger.server;
 
+import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@Log
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+        Application application = run.getBean(Application.class);
+        application.run();
+    }
+
+    private void run() {
+        log.info("Starting camera services");
     }
 }
