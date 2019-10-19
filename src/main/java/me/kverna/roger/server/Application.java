03678d231cc2fc5e1163107f3e2e64c90e056990
Per Christian Kvalvik <pc@kvalvik.no>
2019-10-19 14:01:13 +0200

    Restructure method for running services concurrently

--- src/main/java/me/kverna/roger/server/Application.java
+++ src/main/java/me/kverna/roger/server/Application.java
@@ -1,21 +1,32 @@
 package me.kverna.roger.server;
 
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 
 @SpringBootApplication
 @Log
 public class Application {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
-        Application application = run.getBean(Application.class);
-        application.run();
+        SpringApplication.run(Application.class, args);
     }
 
-    private void run() {
-        log.info("Starting camera services");
+    @Bean(name = "mainExecutor")
+    public TaskExecutor mainExecutor() {
+        return new SimpleAsyncTaskExecutor();
+    }
+
+    @Bean
+    public CommandLineRunner startServices(@Qualifier("mainExecutor") TaskExecutor executor) {
+        return args -> {
+            // TODO: Add services to start
+            // executor.execute(new Service());
+        };
     }
 }
