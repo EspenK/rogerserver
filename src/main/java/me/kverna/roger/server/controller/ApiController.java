3755b933e644191990e0ff0bcc5a44e3e604e669
Per Christian Kvalvik <pc@kvalvik.no>
2019-10-24 13:54:19 +0200

    Create custom controller for API endpoints.

---
+++ src/main/java/me/kverna/roger/server/controller/ApiController.java
@@ -0,0 +1,17 @@
+package me.kverna.roger.server.controller;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@RestController
+@RequestMapping
+public @interface ApiController {
+    @AliasFor(annotation = RequestMapping.class)
+    String value();
+}
