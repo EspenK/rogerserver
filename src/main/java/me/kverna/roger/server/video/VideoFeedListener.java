d61828e5459ded61b70d41d493e3fcc8b67596a8
Per Christian Kvalvik <pc@kvalvik.no>
2019-10-28 17:01:04 +0100

    Refactor video feed service and add error handling

--- src/main/java/me/kverna/roger/server/video/VideoFeedListener.java
+++ src/main/java/me/kverna/roger/server/video/VideoFeedListener.java
@@ -2,6 +2,5 @@ package me.kverna.roger.server.video;
 
 public interface VideoFeedListener {
     void process(byte[] chunk);
-
     boolean isAlive();
 }
--- src/main/java/me/kverna/roger/server/video/VideoFeedService.java
+++ src/main/java/me/kverna/roger/server/video/VideoFeedService.java
@@ -6,6 +6,7 @@ import me.kverna.roger.server.data.Camera;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -20,20 +21,12 @@ public class VideoFeedService implements Runnable {
     private InputStream stream;
     private List<VideoFeedListener> videoFeedListeners;
 
-    private boolean processing = true;
+    private boolean running = true;
 
     public VideoFeedService(Camera camera, int bufferSize) throws IOException {
         this.camera = camera;
         this.bufferSize = bufferSize;
         this.videoFeedListeners = new ArrayList<>();
-
-        // Parse the camera URL and open a connection
-        URL url = new URL(camera.getLocalStreamUrl());
-        log.info(String.format("Opening connection for %s: %s", camera.getName(), url));
-        URLConnection connection = url.openConnection();
-
-        // Set the camera stream
-        this.stream = connection.getInputStream();
     }
 
     public VideoFeedService(Camera camera) throws IOException {
@@ -49,37 +42,106 @@ public class VideoFeedService implements Runnable {
     }
 
     private synchronized void stop() {
-        processing = false;
+        running = false;
     }
 
     @Override
     public void run() {
-        byte[] buffer;
-        
-        while (processing) {
+        while (running) {
+            // Attempt to open a connection
+            log.info(String.format("Opening connection for %s: %s", camera.getName(), camera.getLocalStreamUrl()));
             try {
-                buffer = stream.readNBytes(bufferSize);
-
-                // Send buffer to all listeners and kill dead listeners
-                Iterator<VideoFeedListener> it = videoFeedListeners.iterator();
-                while (it.hasNext()) {
-                    VideoFeedListener listener = it.next();
-                    if (listener.isAlive()) {
-                        listener.process(buffer);
-                    } else {
-                        it.remove();
-                    }
-                }
-            } catch (IOException e) {
-                log.severe("Connection to " + camera.getLocalStreamUrl() + " was lost");
+                openConnectionUntilConnected(60);
+            } catch (InterruptedException e) {
                 stop();
             }
+
+            // Serve data to all listeners
+            serveStreamForever();
+            log.severe("Connection to " + camera.getLocalStreamUrl() + " was lost");
+
+            // Close the stream
+            try {
+                stream.close();
+            } catch (IOException e) {
+                log.severe("Could not close stream for " + camera.getLocalStreamUrl());
+            }
+        }
+    }
+
+    /**
+     * Opens a connection to the camera associated with the VideoFeedService.
+     * It keeps retrying until a connection has been established
+     *
+     * @param sleepSeconds the time to sleep between each connection in seconds
+     * @throws InterruptedException when the connection is interrupted
+     */
+    private void openConnectionUntilConnected(long sleepSeconds) throws InterruptedException {
+        while (true) {
+            try {
+                openConnection();
+                break;
+            } catch (IOException e) {
+                log.warning(String.format("Could not open connection to %s. Retrying in %d seconds.", camera.getLocalStreamUrl(), sleepSeconds));
+                Thread.sleep(sleepSeconds * 1000);
+            }
+        }
+    }
+
+    /**
+     * Opens a connection to the camera associated with the VideoFeedService.
+     *
+     * @throws IOException if the URL can't be opened
+     */
+    private void openConnection() throws IOException {
+        // Parse the camera URL
+        URL url;
+        try {
+            url = new URL(camera.getLocalStreamUrl());
+        } catch (MalformedURLException e) {
+            // Generate a runtime exception for unexpected malformed url, which should not be possible to reach
+            throw new RuntimeException("Camera " + camera + " has malformed URL where it should not be possible.");
         }
 
-        try {
-            stream.close();
-        } catch (IOException e) {
-            log.severe("Could not close stream for " + camera.getLocalStreamUrl());
+        // Open a connection to the camera stream
+        URLConnection connection = url.openConnection();
+
+        // Set the camera stream
+        this.stream = connection.getInputStream();
+    }
+
+    /**
+     * Read the camera stream and serve it to all listeners forever, or
+     * until the stream is closed unexpectedly.
+     */
+    private void serveStreamForever() {
+        byte[] buffer;
+        boolean serving = true;
+
+        while (serving) {
+            try {
+                buffer = stream.readNBytes(bufferSize);
+                sendToListeners(buffer);
+            } catch (IOException ignored) {
+                serving = false;
+            }
+        }
+    }
+
+    /**
+     * Send the given chunk to every listener. Any dead listeners will be removed.
+     *
+     * @param chunk a byte[] of data to be sent to every listener
+     */
+    private void sendToListeners(byte[] chunk) {
+        Iterator<VideoFeedListener> it = videoFeedListeners.iterator();
+        while (it.hasNext()) {
+            VideoFeedListener listener = it.next();
+            if (listener.isAlive()) {
+                listener.process(chunk);
+            } else {
+                it.remove();
+            }
         }
     }
 }
