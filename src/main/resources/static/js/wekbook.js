11ff76d5726784841eb665458e1ffa7085801100
espen <espenk95@gmail.com>
2019-11-18 20:35:02 +0100

    Add a page to show all webhooks with delete button

--- src/main/resources/static/js/wekbook.js
+++ src/main/resources/static/js/wekbook.js
@@ -1,3 +1,25 @@
+function showWebhookList() {
+    location.href = '#webhook';
+    waitForElementFirst('webhookContainer', 50, _showWebhook);
+}
+
+async function _showWebhook(container) {
+    container.innerHTML = '';
+
+    let webhooks = await getWebhooks();
+    webhooks.forEach(showWebhookEntry);
+
+    function showWebhookEntry(webhook) {
+        let section = document.createElement('section');
+
+        section.innerHTML = `
+        <div class="padded">${webhook.url} | 
+        <i onclick="deleteWebhook(${webhook.id})" class="red">delete</i></div>`;
+
+        container.appendChild(section);
+    }
+}
+
 async function addWebhook(form) {
     let webhook = {
         'url': form.url.value
@@ -14,7 +36,7 @@ async function addWebhook(form) {
         let data = await response.json();
         console.log(data.message);
     } else {
-        location.href = '#camera';
+        showWebhookList();
     }
 }
 
@@ -24,6 +46,17 @@ async function deleteWebhook(webhook_id) {
     if (response.status !== 200) {
         console.log(await response.json().message);
     } else {
-        location.href = '#camera';
+        showWebhookList();
+    }
+}
+
+async function getWebhooks() {
+    let response = await fetch_secure('/webhooks', {method: 'GET'});
+
+    if (response.status !== 200) {
+        console.log(await response.json().message);
+        return null;
+    } else {
+        return await response.json();
     }
 }
